e7d3c0c88965cc8c7b15b40efe93a689782162bb
TRag <69841578+ayaka312@users.noreply.github.com>
2022-02-21 13:04:23 +0700

    Update httppost.py

--- httppost.py
+++ httppost.py
@@ -1,4 +1,4 @@
-import socket, sys, argparse
+import socket, html, argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--url")
@@ -6,44 +6,28 @@ parser.add_argument("--user")
 parser.add_argument("--password")
 args = parser.parse_args()
 
-
-def recvall(s):
-    total_data = []
-    response = s.recv(4096)
-    while (len(response) > 0):
-        total_data.append(response.decode())
-        response = s.recv(4096)
-    response = ''.join(total_data)
-    return response
-
-
-def getDomain(url):
-    domain = ""
-    if url[0:8] == "https://":
-        for i in range(8, len(url)):
-            if url[i] == '/':
-                break
-            domain += url[i]
-    if url[0:7] == "http://":
-        for i in range(7, len(url)):
-            if url[i] == '/':
-                break
-            domain += url[i]
-    return domain
-
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 url = args.url
 user = args.user
 password = args.password
-domain = getDomain(url)
-client.connect((domain, 80))
-body = "log="+user+"&pwd="+password+"&wp-submit=Log+In"
-request = "POST /wp-login.php HTTP/1.1\r\n"+"HOST: "+domain + "\r\n"+"Content-Length: "+str(len(body))+"\r\n"+"Content-Type: application/x-www-form-urlencoded"+"\r\n"+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"+"\r\n"+"username-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"+"\r\n" + "Cookie: wordpress_test_cookie=WP Cookie check; wp_lang=en_US"+"\r\n" \
-    "\r\n"+body
-client.send(request.encode())
-response = recvall(client)
-if "HTTP/1.1 302 Found" in response and "is incorrect" not in response and "is not registered on this site" not in response:
-    print("Dang nhap thanh cong.")
+request_body = "log=" + user + "&pwd=" + password + "&wp-submit=Log+In"
+get_url = ""
+i = 8
+if url[0:7] == "http://":
+	i = 7
+get_url += url[i:(len(url)-1)]
+s.connect((get_url, 80))
+request = "POST /wp-login.php HTTP/1.1\r\nHost: " + get_url + "\r\n"
+request += "Content-Length: " + str(len(request_body)) + "\r\n"
+request += "Content-Type: application/x-www-form-urlencoded\r\n"
+request += "\r\n" + request_body
+
+s.send(request.encode())
+
+response = s.recv(2048)
+s.close()
+response = response.decode("utf8")
+if "HTTP/1.1 302 Found" in response and "login_error" not in response:
+	print("User " + user + " dang nhap thanh cong")
 else:
-    print("Dang nhap that bai.")
+	print("User " + user + " dang nhap that bai")
