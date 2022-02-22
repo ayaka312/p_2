05832b9fe1e4a452950e8722398b8ba245ddc5e1
TRag <69841578+ayaka312@users.noreply.github.com>
2022-02-22 08:28:26 +0700

    Update test.py

--- test.py
+++ test.py
@@ -29,35 +29,48 @@ s.send(request.encode())
 response = s.recv(2048)
 s.close()
 response = response.decode("utf8")
+cookie = ""
 if "HTTP/1.1 302 Found" in response and "login_error" not in response:
-	cookie = ""
-    for i in response.split("\r\n"):
-        if "Set-Cookie: " in i:
-            cookie += " " + i.split(" ")[1] 
-    s.connect((get_url, 80))
-    request_cookie = "GET /wp-admin/media-new.php HTTP/1.1\r\nHost: " + get_url + "\r\n" + "Cookie: " + cookie + "\r\n\r\n"
-    s.send(request_cookie.encode())
-    response_cookie = s.recv(2048)
-    res = response_cookie[response_cookie.index("\"_wpnonce\":\"")+12:response_cookie.index("\"_wpnonce\":\"")+22]
-    filename = filepath.split("/")[-1]
-    filetype = filename.split(".")[-1]
-    file = open(filepath, 'rb').read()   
-    request_file = "------WebKitFormBoundary" + "\r\n" + "Content-Disposition: form-data; name=\"name\"" + "\r\n\r\n" + filename + "\r\n"+"------WebKitFormBoundary" + "\r\n" + \
-        "Content-Disposition: form-data; name=\"action\"" + "\r\n\r\n"+"upload-attachment"+"\r\n"+"------WebKitFormBoundary"+"\r\n" + "Content-Disposition: form-data; name=\"_wpnonce\"" + \
-        + "\r\n\r\n" + res  + "\r\n"+"------WebKitFormBoundary" + "\r\n"+"Content-Disposition: form-data; name=\"async-upload\"; filename=\"" + filename + "\"" + "\r\n" + "Content-Type: "+ \
-        + filetype + "\r\n\r\n" + file + "\r\n" + "------WebKitFormBoundary--"
-    request = "POST /wp-admin/async-upload.php HTTP/1.1\r\n"+"Host: " + get_url + "\r\n" 
-    request += 'Cookie:' + cookie + '\r\n'
-    request += "Content-Length: " + str(len(request_file)) + "\r\n"
-    request += "Content-Type: application/x-www-form-urlencoded\r\n"    
-    s.send(request.encode() + request_file.encode())
-    response_upfile = s.recv(2048)
-    if "HTTP/1.1 200 OK" in res:
-		print("Upload thanh cong")
-		file_id = res[res.rfind("\r\n") + 1:].replace("\n","").replace("\r","")
-		return int(file_id)
-	else: 
-		print("Upload that bai")
-		return -1
+	for i in response.split("\r\n"):
+		if "Set-Cookie:" in i:
+                	cookie += " " + i.split(" ")[1] 
+	request_cookie = "GET /wp-admin/media-new.php HTTP/1.1\r\nHost: " + get_url + "\r\n" + "Cookie:" + cookie + "\r\n\r\n"
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((get_url, 80))
+	s.send(request_cookie.encode())
+	response_cookie = s.recv(2048)
+	response_cookie = response_cookie.decode("utf8")
+	response_cookie_index = response_cookie.find("\"_wpnonce\":\"")
+	res = response_cookie[response_cookie_index+ 12:response_cookie_index + 22]
+	filename = filepath.split("/")[-1]
+	filetype = filename.split(".")[-1]
+	file_img = open(filepath, 'rb').read()   
+	request_file = "------WebKitFormBoundary"+"\r\n"+"Content-Disposition: form-data; name=\"name\"" + \
+        "\r\n\r\n"+filename+"\r\n"+"------WebKitFormBoundary"+"\r\n" + \
+        "Content-Disposition: form-data; name=\"action\"" + \
+        "\r\n\r\n"+"upload-attachment"+"\r\n"+"------WebKitFormBoundary"+"\r\n" + \
+        "Content-Disposition: form-data; name=\"_wpnonce\""+"\r\n\r\n"+res+"\r\n"+"------WebKitFormBoundary" + \
+        "\r\n"+"Content-Disposition: form-data; name=\"async-upload\"; filename=\"" + \
+	filename+"\""+"\r\n"+"Content-Type: image/"+filetype+"\r\n\r\n"
+	request_file = request_file.encode()+file_img+b"\r\n"+b"------WebKitFormBoundary--"
+	request = "POST /wp-admin/async-upload.php HTTP/1.1\r\n"+"Host: " + get_url + "\r\n" 
+	request += 'Cookie:' + cookie + '\r\n'
+	request += "Content-Length: " + str(len(request_file)) + "\r\n"
+	request += "Content-Type: application/x-www-form-urlencoded\r\n"
+	#request += request_file    
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((get_url, 80))
+	s.send(request.encode() + request_file)
+	response_upfile = s.recv(2048)
+	response_upfile = response_upfile.decode("utf8")
+	s.close()
+	print("ok")
+	if "HTTP/1.1 200 OK" in response_upfile:
+		print("Upload success\r\nFile upload url: ")
+		response_upfile_start = response_upfile.find("\"url\":\"")
+		response_upfile_end = response_upfile.find("\"")
+		print(response_upfile[response_upfile_start:response_upfile_end])
+	else:
+		print("Upload failed")
 else:
 	print("Upload failed")
