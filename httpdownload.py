473bee7a9ff979fe8e707bdf533e0ed7675d2ec6
TRag <69841578+ayaka312@users.noreply.github.com>
2022-02-22 14:30:33 +0700

    Update httpdownload.py

--- httpdownload.py
+++ httpdownload.py
@@ -1,60 +1,45 @@
-import socket
-import sys
-import argparse
+import socket, argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--url")
 parser.add_argument("--remotefile")
 args = parser.parse_args()
 
-
-def recvall(s):
-    total_data = []
-    response = s.recv(4096)
-    while (len(response) > 0):
-        total_data.append(response)
-        response = s.recv(4096)
-    response = b''.join(total_data)
-    return response
-
-
-def getDomain(url):
-    domain = ""
-    if url[0:8] == "https://":
-        for i in range(8, len(url)):
-            if url[i] == '/':
-                break
-            domain += url[i]
-    if url[0:7] == "http://":
-        for i in range(7, len(url)):
-            if url[i] == '/':
-                break
-            domain += url[i]
-    return domain
-
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 url = args.url
-pathFile = args.remotefile
-domain = getDomain(url)
-client.connect((domain, 80))
-request = "GET "+pathFile+" HTTP/1.1\r\n"+"Host: "+domain+"\r\n"+"\r\n"
-client.send(request.encode())
-response = recvall(client)
-len_image = b""
-if b"HTTP/1.1 200 OK" in response:
-    for i in range(0, len(response)):
-        if len_image != b"":
+filepath = args.remotefile
+image_name = filepath.split("/"[-1])
+get_url = ""
+i = 8
+if url[0:7] == "http://":
+    i = 7
+get_url += url[i:(len(url)-1)]
+s.connect((get_url, 80))
+
+request = "GET " + filepath + " HTTP/1.1\r\nHost: " + get_url + "\r\n\r\n"
+s.send(request.encode())
+
+respons = s.recv(2048)
+s.close()
+response = bytearray()
+while True:
+	if not respons:
+		break
+	response += respons
+	
+response.decode("utf8")
+
+if "HTTP/1.1 200 OK" in response:
+    image_len = ""
+    image_len_start = response.find("Content-Length: ")
+    for j in range(image_len_start + 16, len(response)):
+        if chr(response[j].isdigit == True):
+            image_len += response[j]
+        else:
             break
-        if response[i:i+16] == b"Content-Length: ":
-            for j in range(i+16, len(response)):
-                if(not chr(response[j]).isdigit()):
-                    len_image = response[i+16:j]
-                    break
+    print("Kich thuoc file anh: " + image_len.decode() + "bytes")
+    image_type = response.split("\r\n\r\n")[-1]
+    image_url = "/home/tragserver/Desktop/code/challenge4/" + image_name
+    open(image_url, "wb").write(image_type)
 else:
-    print("Khong ton tai file anh.")
+    print("Khong ton tai file anh")
     exit(0)
-print("Kich thuoc file anh: "+len_image.decode()+" bytes")
-content_file = response.split(b"\r\n\r\n")[1]
-fileName = pathFile.split('/')[-1]
-location = "/home/trang/Desktop/challenge04/"+fileName
-open(location, "wb").write(content_file)
