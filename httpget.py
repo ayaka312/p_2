770eaf4a94d1a6ae78bfbe2a4537372399878f4e
TRag <69841578+ayaka312@users.noreply.github.com>
2022-02-23 14:24:31 +0700

    Update httpget.py

--- httpget.py
+++ httpget.py
@@ -6,16 +6,20 @@ args = parser.parse_args()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 url = args.url
+get_url = ""
 i = 8
 if url[0:7] == "http://":
 	i = 7
 get_url += url[i:(len(url)-1)]
-print(get_url)
 s.connect((get_url, 80))
 request = "GET / HTTP/1.1\r\nHost: "+get_url+"\r\n\r\n"
 s.send(request.encode())
-
-response = s.recv(2048)
+response = b''
+while True:
+	respons = s.recv(2048)
+	if not respons:
+		break
+	response += respons
 s.close()
 response = response.decode("utf8")
 title_start = response.find("<title>")
